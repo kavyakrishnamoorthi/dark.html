78d5caddd8ac465585264ade3001ea1848f34332
kavyakrishnamoorthi <147286881+kavyakrishnamoorthi@users.noreply.github.com>
2023-10-08 06:58:06 +0530

    Add files via upload

---
+++ script.jss.js
@@ -0,0 +1,14 @@
+function toggleDarkMode() {
+    const body = document.body;
+    body.classList.toggle("dark-mode");
+}
+
+
+const themeToggle = document.getElementById("theme-toggle");
+themeToggle.addEventListener("click", toggleDarkMode);
+
+
+const prefersDarkMode = window.matchMedia("(prefers-color-scheme: dark)").matches;
+if (prefersDarkMode) {
+    toggleDarkMode();
+}
